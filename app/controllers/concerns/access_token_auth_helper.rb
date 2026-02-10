13648cdd93dd33349454236c7eb773d512e70f66
sirius <sirius@siriusdeMacBook-Pro.local>
2026-02-10 17:40:03 +0900

    feat(bot): allow message index access for bot token

--- app/controllers/concerns/access_token_auth_helper.rb
+++ app/controllers/concerns/access_token_auth_helper.rb
@@ -1,7 +1,7 @@
 module AccessTokenAuthHelper
   BOT_ACCESSIBLE_ENDPOINTS = {
     'api/v1/accounts/conversations' => %w[toggle_status toggle_priority create update custom_attributes],
-    'api/v1/accounts/conversations/messages' => ['create'],
+    'api/v1/accounts/conversations/messages' => %w[create index],
     'api/v1/accounts/conversations/assignments' => ['create']
   }.freeze
 
